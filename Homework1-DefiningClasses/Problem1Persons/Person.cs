b9f66310185beb153bb903c600ac3ff173704c9c
IskraNikolova <air_iskra@yahoo.com>
2015-11-17 14:01:47 +0200

    add first project

---
+++ Homework1-DefiningClasses/Problem1Persons/Person.cs
@@ -0,0 +1,85 @@
+﻿
+using System;
+
+public class Person
+{
+    private string name;
+    private int age;
+    private string email;
+
+    public Person(string name, int age, string email)
+    {
+        this.Name = name;
+        this.Age = age;
+        this.Email = email;
+    }
+
+    public Person(string name, int age)
+        : this(name, age, null)
+    {
+    }
+
+    public string Name
+    {
+        get
+        {
+            return this.name;
+        }
+
+        set
+        {
+           
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("Name can not be null.");
+            }
+            this.name = value;
+        }
+    }
+
+    public int Age
+    {
+        get
+        {
+            return this.age;
+        }
+
+        set
+        {
+          
+            if (value < 1 || value > 100)
+            {
+                throw new ArgumentException("Invalid age!");
+            }
+            this.age = value;
+        }
+    }
+
+    public string Email
+    {
+        get
+        {
+            return this.email;
+        }
+
+        set
+        {           
+            if (value != null && !value.Contains("@"))
+            {
+                throw new ArgumentException("Invalid email!");
+            }
+            this.email = value;
+        }
+    }
+
+    public override string ToString()
+    {
+        string result = String.Empty;
+
+        result = this.Email != null ? $"Name: {this.Name}, Age: {this.Age}, Email: {this.Email}" :
+                                      $"Name: {this.Name}, Age: {this.Age}";
+
+        return result;
+    }
+}
+
---
+++ Homework1-DefiningClasses/Problem1Persons/PersonMain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class PersonMain
+{
+    public static void Main()
+    {
+        Console.Write("Enter name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter age:");
+        int age = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter email: ");
+        string email = Console.ReadLine();
+
+        Person person = new Person(name, age, email);
+        Person personWithoutEmail = new Person(name, age);
+
+        Console.WriteLine(person + "\n********************\n" + personWithoutEmail);
+    }
+}
+
