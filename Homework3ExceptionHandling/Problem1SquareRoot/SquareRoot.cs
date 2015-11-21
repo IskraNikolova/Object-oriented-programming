1b2bf13d98958ff2f17b70cb1d5f8b900e3b52ee
IskraNikolova <air_iskra@yahoo.com>
2015-11-21 11:54:45 +0200

    new homework

---
+++ Homework3ExceptionHandling/Problem1SquareRoot/SquareRoot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class SquareRoot
+{
+    public static void Main()
+    {
+        try
+        {
+            double inputNumber = double.Parse(Console.ReadLine());
+            double squareRoot = Math.Sqrt(inputNumber);
+            if (inputNumber < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            Console.WriteLine(squareRoot);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid number!");
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            Console.WriteLine("Invalid number!");
+        }
+        finally
+        {
+            Console.WriteLine("Good bye!");
+        }
+    }
+}
+
---
+++ Homework3ExceptionHandling/Problem2EnterNumbers/EnterNumbers.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class EnterNumbers
+{
+    private const int Count = 10;
+    private const int MinNumber = 1;
+    private const int MaxNumber = 100;
+
+    private static List<int> result; 
+    public static void Main()
+    {
+        result = new List<int>();
+        while (result.Count < Count)
+        {
+            try
+            {
+                int number = ReadNumber(MinNumber, MaxNumber);
+                int number2 = ReadNumber(result[result.Count - 1], MaxNumber);
+                result.Add(!result.Any() ? number : number2);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Number must be in range[1...100]");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number!Try again!");
+            }
+        }
+        
+        Console.WriteLine(string.Join(", ", result));      
+    }
+
+    public static int ReadNumber(int start, int end)
+    {
+        Console.Write("Enter a number in range [{0}...{1}] ", start + 1, end - 1);
+        int number = int.Parse(Console.ReadLine());
+        if (number <= start || number >= end)
+        {
+            throw new ArgumentOutOfRangeException($"Number must be in range[{start + 1}...{end - 1}]");
+        }
+        return number;
+    }
+}
+
