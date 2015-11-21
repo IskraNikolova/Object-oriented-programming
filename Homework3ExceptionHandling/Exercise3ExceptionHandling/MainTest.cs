3c2573e681301cc4a16826697dd524fcff46685a
IskraNikolova <air_iskra@yahoo.com>
2015-11-21 15:46:03 +0200

    add exercise 3

---
+++ Homework3ExceptionHandling/Exercise3ExceptionHandling/MainTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercise3ExceptionHandling
+{
+    public class MainTest
+    {
+        public static void Main()
+        {
+            try
+            {
+                  Person unNamed = new Person(string.Empty, "SecondName", 31);
+               // Person negativeAge = new Person("Pesho", "SecondName", -32);
+               // Person unNamedSecond = new Person("FirstName", string.Empty, 31);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("Exception thrown: {0}", ex.Message);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Exception thrown: {0}", ex.Message);
+            }
+        }
+    }
+}
---
+++ Homework3ExceptionHandling/Exercise3ExceptionHandling/Person.cs
@@ -0,0 +1,65 @@
+﻿
+using System;
+
+namespace Exercise3ExceptionHandling
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+
+        public string FirstName {
+            get
+            {
+                return this.firstName;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("value", "First name can not be empty");
+                }
+                this.firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("value", "Last name can not be empty");
+                }
+                this.lastName = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                if (value <= 0 || value > 100)
+                {
+                    throw new ArgumentOutOfRangeException("value", "Age must be in range[1...120]");
+                }
+                this.age = value;
+            }
+        }
+    }
+}
+
