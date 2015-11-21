22dd648de8dac84fa9061af680f3aeccd60d77ae
IskraNikolova <air_iskra@yahoo.com>
2015-11-21 13:53:23 +0200

    add exercise

---
+++ Homework1-DefiningClasses/ExerciseDefiningClasse/Dog.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+public class Dog
+{
+    private string name;
+    private string breed;
+
+    public Dog() : this(null, null)
+    {
+    }
+
+    public Dog(string name, string breed)
+    {
+        this.Name = name;
+        this.Breed = breed;
+    }
+
+    public string Name { get; set; }
+    public string Breed { get; set; }
+
+    public void Bark()
+    {
+        Console.WriteLine("{0} ({1}) said: Bauuuuuuuuu!", this.Name ?? 
+            "[unnamed dog]", this.Breed ?? "[unknow breed]");
+    }
+}
+
---
+++ Homework1-DefiningClasses/ExerciseDefiningClasse/ExerciseDefiningClasse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class ExerciseDefiningClasse
+{
+    public static void Main()
+    {
+        Dog sharo = new Dog("Sharo", "bolkonka");
+        Dog unnamed = new Dog();
+        unnamed.Name = "NewName";
+
+        sharo.Bark();
+        unnamed.Bark();
+
+    }
+}
+
