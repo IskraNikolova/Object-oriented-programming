f4c007aeb23c4d480a7a92bf7b2bef306f5d5f76
IskraNikolova <air_iskra@yahoo.com>
2015-12-02 15:14:15 +0200

    Add massEffect

---
+++ Skeleton/MassEffect/GameObjects/Projectiles/Laser.cs
@@ -0,0 +1,23 @@
+﻿using MassEffect.Interfaces;
+
+namespace MassEffect.GameObjects.Projectiles
+{
+    public class Laser : Projectile
+    {
+        public Laser(int damage) 
+            : base(damage)
+        {
+        }
+
+        public override void Hit(IStarship ship)
+        {
+            int diff = this.Damage - ship.Shields;
+            ship.Shields -= this.Damage;
+
+            if (diff > 0)
+            {
+                ship.Health -= diff;
+            }           
+        }
+    }
+}
---
+++ Skeleton/MassEffect/GameObjects/Ships/Cruiser.cs
@@ -0,0 +1,24 @@
+﻿using MassEffect.GameObjects.Locations;
+using MassEffect.GameObjects.Projectiles;
+using MassEffect.Interfaces;
+
+namespace MassEffect.GameObjects.Ships
+{
+    public class Cruiser : Starship
+    {
+        private const int CruiserHealth = 100;
+        private const int CruiserShields = 100;
+        private const int CruiserDamage = 50;
+        private const int CruiserFuel = 300;
+
+        public Cruiser(string name, StarSystem location)
+            : base(name, CruiserHealth, CruiserShields, CruiserDamage, CruiserFuel, location)
+        {
+        }
+
+        public override IProjectile ProduceAttack()
+        {
+            return new PenetrationShell(this.Damage);
+        }
+    }
+}
