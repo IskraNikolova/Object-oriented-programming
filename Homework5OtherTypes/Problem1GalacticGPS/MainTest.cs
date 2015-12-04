2a705691d4d839885290877c9077eb0cb58d7fb5
IskraNikolova <air_iskra@yahoo.com>
2015-12-04 10:59:27 +0200

    update

--- Homework5OtherTypes/Problem1GalacticGPS/MainTest.cs
+++ Homework5OtherTypes/Problem1GalacticGPS/MainTest.cs
@@ -6,7 +6,7 @@ namespace Problem1GalacticGPS
     {
         public static void Main()
         {
-            double latitude = 218.037986;
+            double latitude = 18.037986;
             double longitude = 28.870097;
 
             Location home = new Location(latitude, longitude, Planet.Earth);
