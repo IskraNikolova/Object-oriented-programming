2890b274a0821a81d0f319629ce42f6a2add8f2b
IskraNikolova <air_iskra@yahoo.com>
2015-12-04 10:59:01 +0200

    Add new project

---
+++ Homework5OtherTypes/Problem2FractionCalculator/Fraction.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+using System.Runtime;
+
+namespace Problem2FractionCalculator
+{
+    public struct Fraction
+    {
+        private decimal numerator;
+        private decimal denominator;
+
+        public Fraction(decimal numerator, decimal denominator) 
+            : this()      
+        {
+            this.Numerator = numerator;
+            this.Denominator = denominator;
+        }
+
+        public decimal Numerator { get; set; }
+        public decimal Denominator { get; set; }
+
+        public static Fraction operator +(Fraction f1, Fraction f2)
+        {
+            decimal newNumerator = (f1.Numerator*f2.Denominator) + (f2.Numerator*f1.Denominator);
+            decimal newDenumerator = (f1.Denominator*f2.Denominator);
+            return new Fraction(newNumerator, newDenumerator);
+        }
+
+        public static Fraction operator -(Fraction f1, Fraction f2)
+        {
+            decimal newNumerator = (f1.Numerator * f2.Denominator) - (f2.Numerator * f1.Denominator);
+            decimal newDenumerator = (f1.Denominator * f2.Denominator);
+            return new Fraction(newNumerator, newDenumerator);
+        }
+
+        public override string ToString()
+        {
+            decimal res = this.Numerator/this.Denominator;
+            string result = res.ToString();
+            return result;
+        }
+    }
+}
