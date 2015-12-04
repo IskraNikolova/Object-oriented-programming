2b6622834fad1dc6e341e8337c91b1b1c2382cc8
IskraNikolova <air_iskra@yahoo.com>
2015-12-04 13:05:10 +0200

    Add a new project

---
+++ Homework5OtherTypes/Problem3GenericList/GenericList.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Text;
+
+namespace Problem3GenericList
+{
+    public class GenericList<T>
+        where T : IComparable<T>
+    {
+        private const int DefaultCapacity = 16;
+        private T[] array;
+        private int index;
+        private int currentCapacity;
+
+        public GenericList(int capacity = DefaultCapacity)
+        {
+            this.array = new T[capacity];
+            this.currentCapacity = capacity;
+            this.index = 0;
+        }
+
+        /// <summary>
+        /// Length of collection
+        /// </summary>
+        public int Length
+        {
+            get { return this.index; }
+        }
+
+        /// <summary>
+        /// Add value on the collection
+        /// </summary>
+        /// <param name="item">value added</param>
+        public void Add(T item)
+        {
+            this.array[this.index] = item;
+            this.index++;
+            if (this.index == currentCapacity)
+            {
+                this.Resize(this.currentCapacity * 2);
+            }
+        }
+
+        /// <summary>
+        /// Remove value of the position
+        /// </summary>
+        /// <param name="index">Index of value</param>
+        public void Remove(int index)
+        {
+            this.array[index] = default(T);
+            for (int i = index; i < this.array.Length - 1; i++)
+            {
+                this.array[i] = this.array[i + 1];
+            }
+            this.index--;
+        }
+
+        /// <summary>
+        /// Add value on index
+        /// </summary>
+        /// <param name="index">index of add</param>
+        /// <returns>Return value of this index</returns>
+        public T this[int index]
+        {
+            get
+            {
+                return this.array[index];
+            }
+            set
+            {
+                this.array[index] = value;
+                this.index++;
+            }
+        }
+
+        /// <summary>
+        /// Insert value of index
+        /// </summary>
+        /// <param name="index">Index where we insert</param>
+        /// <param name="value">Value for insert</param>
+        public void Insert(int index, T value)
+        {
+            T[] arrayCopy = new T[currentCapacity];
+            for (int i = 0; i < currentCapacity; i++)
+            {
+                arrayCopy[i] = this.array[i];
+            }
+
+            for (int i = index + 1; i < this.array.Length; i++)
+            {  
+                T newValue = arrayCopy[i - 1];
+                this.array[i] = newValue;
+            }
+            this.array[index] = value;
+            this.index++;
+        }
+
+        /// <summary>
+        /// Clear all collection
+        /// </summary>
+        public void Clear()
+        {
+            for (int i = 0; i < currentCapacity; i++)
+            {
+                this.array[i] = default(T);
+            }
+            index = 0;
+        }
+
+        /// <summary>
+        /// Find index of item
+        /// </summary>
+        /// <param name="item">Value</param>
+        /// <returns>Index</returns>
+        public int Find(T item)
+        {
+            int findIndex = -1;
+            for (int i = 0; i < index; i++)
+            {
+                if (item.Equals(this.array[i]))
+                {
+                    findIndex = i;
+                }
+            }
+            return findIndex;
+        }
+
+        /// <summary>
+        /// Contains value of collection 
+        /// </summary>
+        /// <param name="item">Search value</param>
+        /// <returns>False or True</returns>
+        public bool Contains(T item)
+        {
+            bool isContains = false;
+            for (int i = 0; i < index; i++)
+            {
+                if (item.Equals(this.array[i]))
+                {
+                    isContains = true;
+                }
+            }
+            return isContains;
+        }
+
+        public override string ToString()
+        {
+            T[] resultaArray = new T[index];
+            for (int i = 0; i < index; i++)
+            {
+                resultaArray[i] = this.array[i];
+            }
+
+            string result = "[" + String.Join(", ", resultaArray) + "]";
+            return result;
+        }
+
+        /// <summary>
+        /// Resize collection
+        /// </summary>
+        /// <param name="newCapacity">new size of capacity</param>
+        private void Resize(int newCapacity)
+        {
+            T[] newArray = new T[newCapacity];
+            for (int i = 0; i < this.index; i++)
+            {
+                newArray[i] = this.array[i];
+                this.array = newArray;
+                this.currentCapacity = newCapacity;
+            }
+        }       
+    }
+}
---
+++ Homework5OtherTypes/Problem3GenericList/MainTest.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Problem3GenericList
+{
+    public class MainTest
+    {
+        public static void Main()
+        {
+            GenericList<string> names = new GenericList<string>();
+            names.Add("Pipi");
+            names.Add("Lili");
+            names.Add("Bobo");
+            names.Add("Miroslav");
+
+            Console.WriteLine(names); //Print GenericList with override ToString()
+            Console.WriteLine();
+
+            Console.WriteLine($"Length of list is {names.Length}."); // collection has length
+            Console.WriteLine();
+
+            names.Remove(0);
+            Console.WriteLine(names);
+            Console.WriteLine();
+
+            names.Insert(2, "New name - Isi");
+            Console.WriteLine(names);
+
+            Console.WriteLine();
+            Console.WriteLine(names.Contains("Bobo") + " - that contains");
+
+            Console.WriteLine();
+            Console.WriteLine($"Index of this value is {names.Find("Lili")}.");
+            Console.WriteLine();
+
+            names.Clear();
+            Console.WriteLine(names + " - Collection is clear");
+            Console.WriteLine();
+
+            GenericList<int> ints = new GenericList<int>();
+            ints[0] = 1; // add value on the position
+            ints[1] = 2;
+            ints[2] = 3;
+            ints[3] = 4;
+       
+            Console.WriteLine("Min value is " + Min(ints));
+            Console.WriteLine("Max value is " + Max(ints));
+            
+        }
+
+        /// <summary>
+        /// Search min value
+        /// </summary>
+        /// <typeparam name="T">all types</typeparam>
+        /// <param name="list">GenericList of all types</param>
+        /// <returns>min value</returns>
+        public static T Min<T>(GenericList<T> list)
+            where T : IComparable<T>
+        {
+            T min = list[0];
+            for (int i = 0; i < list.Length; i++)
+            {
+                if (list[i].CompareTo(min) < 0)
+                {
+                    min = list[i];
+                }
+            }
+            return min;
+        }
+
+        /// <summary>
+        /// Search max value
+        /// </summary>
+        /// <typeparam name="T">all types</typeparam>
+        /// <param name="list">GenericList of all types</param>
+        /// <returns></returns>
+        public static T Max<T>(GenericList<T> list)
+       where T : IComparable<T>
+        {
+            T max = list[0];
+            for (int i = 0; i < list.Length; i++)
+            {
+                if (list[i].CompareTo(max) > 0)
+                {
+                    max = list[i];
+                }
+            }
+            return max;
+        }
+    }
+}
