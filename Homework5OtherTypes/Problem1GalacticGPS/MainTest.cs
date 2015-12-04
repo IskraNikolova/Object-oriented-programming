16fd06f459999fff66d0673c30f2c968c1ff8014
IskraNikolova <air_iskra@yahoo.com>
2015-12-04 10:20:06 +0200

    Add new project

---
+++ Homework5OtherTypes/Problem1GalacticGPS/MainTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Problem1GalacticGPS
+{
+    public class MainTest
+    {
+        public static void Main()
+        {
+            double latitude = 218.037986;
+            double longitude = 28.870097;
+
+            Location home = new Location(latitude, longitude, Planet.Earth);
+            Console.WriteLine(home);
+        }
+    }
+}
