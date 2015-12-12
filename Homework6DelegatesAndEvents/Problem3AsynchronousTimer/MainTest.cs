49daded8b09bc713eaf30ad7767b2911c7f905f5
IskraNikolova <air_iskra@yahoo.com>
2015-12-12 13:44:56 +0200

    new problem in this project

---
+++ Homework6DelegatesAndEvents/Problem3AsynchronousTimer/MainTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Problem3AsynchronousTimer
+{
+    public class MainTest
+    {
+        public static void Main()
+        {
+            var asyncTimer = new AsyncTimer(Console.WriteLine, 13, 500, "Some message");
+            
+            asyncTimer.Run();
+        }
+    }
+}
