3dc799a2c94d90755062e2532f4099086fdf715b
IskraNikolova <air_iskra@yahoo.com>
2016-03-23 08:30:51 +0200

    add one more exercise

---
+++ 04. OOP-Encapsulation-and-Polymorphism-Exercise-3/BookStore/BookStoreMain.cs
@@ -0,0 +1,20 @@
+﻿using BookStore.Interfaces;
+using BookStore.UI;
+
+namespace BookStore
+{
+    using Engine;
+
+    public class BookStoreMain
+    {
+        public static void Main()
+        { 
+            IInputHandler input = new ConsoleInputHandler();
+            IRenderer renderer = new ConsoleRenderer();
+
+            BookStoreEngine engine = new BookStoreEngine(input, renderer);
+
+            engine.Run();
+        }
+    }
+}
---
+++ 04. OOP-Encapsulation-and-Polymorphism-Exercise-3/BookStore/UI/ConsoleRenderer.cs
@@ -0,0 +1,13 @@
+﻿namespace BookStore.UI
+{
+    using System;
+    using BookStore.Interfaces;
+
+    public class ConsoleRenderer : IRenderer
+    {
+        public void WriteLine(string message, params object[] parameters)
+        {
+            Console.WriteLine(message, parameters);
+        }
+    }
+}
